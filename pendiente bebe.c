3c5f47ec78abe2613c49d116ea253ade29e1174e
zum-15 <bautistazunny@gmail.com>
2026-04-23 12:53:14 -0600

    Add files via upload

---
+++ pendiente bebe.c
@@ -0,0 +1,37 @@
+int main() {
+	int opcion;
+	printf("MENU DE FINANZAS\n");
+	printf("1, IVA\n");
+	printf("2,ISR\n");
+	printf("3, precio + IVA\n"); 
+	printf("4, total de productos\n");
+	printf("5, total de productos + IVA\n:");
+	printf("6, promedio de resto\n");
+	printf("salir\n");
+	printf("selecciona una opcion:");
+	scanf("%d", &opcion);
+	
+	switch (opcion){
+		case 1:
+			printf("tu iva es de:\n");
+			scanf( IVA=valori*. 16);
+			break;
+			case 2:
+				printf("su ISR\n");
+				scanf(ISR = valor2 *.30);
+				break;
+				case 3:
+					printf(" su total de productos es:\n");
+					scanf(total = valor1*1.16);
+					break;
+					case 4:
+						printf("saliendo del sistema\n");
+						scanf()
+						break;
+						
+						default:
+							printf("opcion invalida\n");
+						
+	}
+	return 0;
+} 
